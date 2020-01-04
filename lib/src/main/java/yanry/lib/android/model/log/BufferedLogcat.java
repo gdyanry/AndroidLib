93e4b0e0442397d59fa8c3387d45c4b4b91083d2
rongyu.yan <rongyu.yan@tcl.com>
2020-01-04 16:26:07 +0800

    BufferedLogcat添加漏日志重刷特性

--- lib/src/main/java/yanry/lib/android/model/log/BufferedLogcat.java
+++ lib/src/main/java/yanry/lib/android/model/log/BufferedLogcat.java
@@ -2,7 +2,6 @@ package yanry.lib.android.model.log;
 
 import android.util.Log;
 
-import yanry.lib.java.model.log.Logger;
 import yanry.lib.java.model.schedule.Display;
 import yanry.lib.java.model.schedule.Scheduler;
 import yanry.lib.java.model.schedule.SchedulerManager;
@@ -19,27 +18,27 @@ public class BufferedLogcat extends BufferedStringDisplay {
     private long minFlushInterval;
     private Scheduler bufferScheduler;
     private Scheduler regularizeScheduler;
-    private Logger schedulerLogger;
 
     /**
      * @param manager          应特别注意此SchedulerManager所使用的Logger不可再添加当前LogHandler！
      * @param logcatLevel      日志在logcat中输出所使用的等级，可用值为{@link Log}中定义的等级常量。
      * @param logcatTag        日志在logcat中输出所使用的tag。
      * @param maxIdleTime      最长闲置时间，超出此时间未写入新的日志则将缓冲已有的日志输出到Logcat中。
-     * @param minFlushInterval 往Logcat中输出日志的最短间隔。
+     * @param minFlushInterval 往Logcat中输出日志的最短间隔。当此值大于0遇到Logcat漏刷的情况时会自动重刷。
      */
     public BufferedLogcat(SchedulerManager manager, int logcatLevel, String logcatTag, long maxIdleTime, long minFlushInterval) {
         super(LogcatHandler.MAX_LOG_LEN, "--->>>" + System.lineSeparator(), System.lineSeparator(), true);
         this.logcatLevel = logcatLevel;
         this.logcatTag = logcatTag;
         this.maxIdleTime = maxIdleTime;
-        this.minFlushInterval = minFlushInterval;
         bufferScheduler = manager.get(this);
         bufferScheduler.setDisplay(BufferedLogcat.class, this);
-        LogcatRegularizeDisplay regularizeDisplay = new LogcatRegularizeDisplay();
-        regularizeScheduler = manager.get(regularizeDisplay);
-        regularizeScheduler.setDisplay(LogcatRegularizeDisplay.class, regularizeDisplay);
-        schedulerLogger = manager.getLogger();
+        if (minFlushInterval > 0) {
+            this.minFlushInterval = minFlushInterval;
+            LogcatRegularizeDisplay regularizeDisplay = new LogcatRegularizeDisplay();
+            regularizeScheduler = manager.get(regularizeDisplay);
+            regularizeScheduler.setDisplay(LogcatRegularizeDisplay.class, regularizeDisplay);
+        }
     }
 
     public void printLog(String logContent) {
@@ -56,23 +55,7 @@ public class BufferedLogcat extends BufferedStringDisplay {
             ShowData data = new ShowData().setExtra(segment).setDuration(minFlushInterval).setStrategy(ShowData.STRATEGY_APPEND_TAIL);
             regularizeScheduler.show(data, LogcatRegularizeDisplay.class);
         } else {
-            flush(segment);
-        }
-    }
-
-    private void flush(String msg) {
-        int len = msg.length();
-        int printed = 0;
-        while (true) {
-            printed = Log.println(logcatLevel, logcatTag, msg);
-            if (printed < len) {
-                if (schedulerLogger != null) {
-                    schedulerLogger.ww("flush message to logcat failed: msgLen=", len, ", printed=", printed);
-                }
-                Thread.yield();
-            } else {
-                break;
-            }
+            Log.println(logcatLevel, logcatTag, segment);
         }
     }
 
@@ -83,7 +66,12 @@ public class BufferedLogcat extends BufferedStringDisplay {
 
         @Override
         protected void show(ShowData data) {
-            flush(data.getExtra().toString());
+            String msg = data.getExtra().toString();
+            if (Log.println(logcatLevel, logcatTag, msg) < 0) {
+                // 失败重试
+                regularizeScheduler.show(new ShowData().setExtra(msg).setDuration(minFlushInterval).setStrategy(ShowData.STRATEGY_INSERT_HEAD),
+                        LogcatRegularizeDisplay.class);
+            }
         }
     }
 }
