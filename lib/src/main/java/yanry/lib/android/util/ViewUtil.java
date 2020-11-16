d58553814045b379f8ad9197f36271a928248f02
yanry <rongyu.yan@tcl.com>
2020-11-16 09:39:58 +0800

    修复ViewUtil.hideKeyboard()

--- lib/src/main/java/yanry/lib/android/util/ViewUtil.java
+++ lib/src/main/java/yanry/lib/android/util/ViewUtil.java
@@ -16,57 +16,58 @@ import android.widget.ImageView;
  */
 public class ViewUtil {
 
-	public static boolean switchAnimation(ImageView iv, boolean start) {
-		AnimationDrawable anim = (AnimationDrawable) iv.getDrawable();
-		if (anim != null) {
-			if (start) {
-				if (!anim.isRunning()) {
-					anim.start();
-					return true;
-				}
-			} else {
-				if (anim.isRunning()) {
-					anim.stop();
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    public static boolean switchAnimation(ImageView iv, boolean start) {
+        AnimationDrawable anim = (AnimationDrawable) iv.getDrawable();
+        if (anim != null) {
+            if (start) {
+                if (!anim.isRunning()) {
+                    anim.start();
+                    return true;
+                }
+            } else {
+                if (anim.isRunning()) {
+                    anim.stop();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-	public static void hideKeyboard(View v) {
-		InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-		imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-	}
+    public static void hideKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
 
-	public static void hideKeyboard(Activity activity) {
-		((InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
-				activity.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-	}
+    public static void hideKeyboard(Activity activity) {
+        View currentFocus = activity.getCurrentFocus();
+        if (currentFocus != null) {
+            ((InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
+                    currentFocus.getWindowToken(), InputMethodManager.HIDE_IMPLICIT_ONLY);
+        }
+    }
 
-	/**
-	 * 
-	 * @param v
-	 * @param xy
-	 *            an array of two integers in which to hold the coordinates.
-	 * @return the given array, or a new created array if it's null
-	 */
-	public static int[] getLocationOnScreen(View v, int[] xy) {
-		if (xy == null) {
-			xy = new int[2];
-		}
-		v.getLocationOnScreen(xy);
-		return xy;
-	}
-	
-	public static Activity getActivityByView(View v) {
-		Context context = v.getContext();
-		if (context instanceof Activity) {
-			return (Activity) context;
-		}
-		if (context instanceof ContextWrapper) {
-			return (Activity) ((ContextWrapper) context).getBaseContext();
-		}
-		return null;
-	}
+    /**
+     * @param v
+     * @param xy an array of two integers in which to hold the coordinates.
+     * @return the given array, or a new created array if it's null
+     */
+    public static int[] getLocationOnScreen(View v, int[] xy) {
+        if (xy == null) {
+            xy = new int[2];
+        }
+        v.getLocationOnScreen(xy);
+        return xy;
+    }
+
+    public static Activity getActivityByView(View v) {
+        Context context = v.getContext();
+        if (context instanceof Activity) {
+            return (Activity) context;
+        }
+        if (context instanceof ContextWrapper) {
+            return (Activity) ((ContextWrapper) context).getBaseContext();
+        }
+        return null;
+    }
 }
